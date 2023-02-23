7a7827ac881644b80f6d620b841e45e6e270bcd8
bostonsinaga <bostonsinga@gmail.com>
2023-02-23 22:41:14 +0700

    array parameters returned as specific parameters

--- tools/writer.js
+++ tools/writer.js
@@ -14,12 +14,13 @@ exports.newFile = (
             `** HTML ${TASK_TITLE_STRING} OVERWRITE SAFETY!\n` +
             "**** recommended to write into new file instead\n" +
             "**** otherwise if you intended to do so\n" +
-            "**** please pass into the function as 'writeFunction(HTML_FILE_DIR, NEW_FILE_DIR, IS_OVERWRITE)'"
+            "**** please pass into the function as 'writeFunction(.., IS_OVERWRITE)'"
         );
         console.log(`** HTML ${TASK_TITLE_STRING} WRITE FAILED!`);
     }
     else {
         const mainHTML = CALLBACK_OBJECT(HTML_FILE_DIR);
+        
         if (mainHTML != "") {
             fs.writeFileSync(NEW_FILE_DIR, mainHTML);
             console.log(`** HTML ${TASK_TITLE_STRING} WRITE COMPLETED!`);
