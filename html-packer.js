0434990eb4be07bff0afb7a5708659253b24e687
bostonsinaga <bostonsinga@gmail.com>
2023-02-24 20:14:58 +0700

    add 'html-packer.js' as a hub for modules from './modules/' folder

---
+++ html-packer.js
@@ -0,0 +1,9 @@
+const HTMLNoComment = require("./modules/html-no-comment");
+const HTMLShrinker = require("./modules/html-shrinker");
+const HTMLWrapper = require("./modules/html-wrapper");
+
+module.exports = {
+    HTMLNoComment,
+    HTMLShrinker,
+    HTMLWrapper
+};
